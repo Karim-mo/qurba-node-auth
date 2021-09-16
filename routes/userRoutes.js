dcab231c67509f4fa7436755dc73808ef5ded99e
Karim-mo <karim.mohamed420@gmail.com>
2021-09-16 20:45:27 +0200

    user login & register

---
+++ routes/userRoutes.js
@@ -0,0 +1,123 @@
+import express from 'express';
+import asyncHandler from 'express-async-handler';
+import { body } from 'express-validator';
+import { FormValidation, Auth, Database } from 'qurba-node-common';
+
+// Grab the user schema
+const User = Database.Schemas.User;
+
+// Assign a reference to avoid using the long name every time
+const validate = FormValidation.validate;
+
+const router = express.Router();
+
+const login = asyncHandler(async (req, res) => {
+	const { mobile, username, email, password } = req.body;
+
+	const dbQuery = email ?? username ?? mobile;
+
+	if (!dbQuery) {
+		res.status(400);
+		throw new Error('Invalid Request');
+	}
+
+	const user = await User.findOne({
+		$or: [{ email: dbQuery }, { username: dbQuery }, { mobile: dbQuery }],
+	});
+
+	// Use our schema custom function to compare hashes
+	if (user && (await user.matchPassword(password))) {
+		/*
+		 * To explain the ternary and nullish coalescing operator use:
+		 * To ensure we can't have someone sending requests through an app like postman and bypassing the frontend
+		 * We basically try to make sure the database only takes 1 input out of the 3, incase 2 or more are provided
+		 * We nullify the rest of the fields and since they are sparsed, mongo wont index them during queries on that field
+		 */
+		res.json({
+			name: user.name,
+			email: email ?? null,
+			username: email?.length > 0 ? null : username ?? null,
+			mobile: email?.length > 0 || username?.length > 0 ? null : mobile ?? null,
+			joinedOn: user.createdAt,
+			id: user._id,
+			token: Auth.UserAuth_JWT.signToken(user),
+		});
+	} else {
+		// Never send a 404 on login to avoid brute forcing accounts
+		res.status(400);
+		throw new Error('Check your login credentials.');
+	}
+});
+
+const register = asyncHandler(async (req, res) => {
+	const { name, mobile, username, email, password } = req.body;
+
+	// Check which credentials were used
+	const dbQuery = email ?? username ?? mobile;
+
+	if (!dbQuery) {
+		res.status(400);
+		throw new Error('Invalid Request');
+	}
+
+	const user = await User.findOne({
+		$or: [{ email: dbQuery }, { username: dbQuery }, { mobile: dbQuery }],
+	});
+
+	if (user) {
+		res.status(400);
+		throw new Error('User already exists');
+	} else {
+		const user = await User.create({
+			name,
+			email: email ?? null,
+			username: email?.length > 0 ? null : username ?? null,
+			mobile: email?.length > 0 || username?.length > 0 ? null : mobile ?? null,
+			password,
+		});
+
+		if (user) {
+			res.json({
+				name: user.name,
+				email: email ?? null,
+				username: email?.length > 0 ? null : username ?? null,
+				mobile: email?.length > 0 || username?.length > 0 ? null : mobile ?? null,
+				joinedOn: user.createdAt,
+				id: user._id,
+				token: Auth.UserAuth_JWT.signToken(user),
+			});
+		} else {
+			res.status(500);
+			throw new Error('An unknown error occured while registering, please try again later');
+		}
+	}
+});
+
+router
+	.route('/register')
+	.post(
+		asyncHandler(
+			validate([
+				body('mobile').isNumeric().isMobilePhone().optional(),
+				body('username').isAlphanumeric().optional(),
+				body('email').isEmail().optional(),
+				body('password').isLength({ min: 8 }).isStrongPassword(),
+			])
+		),
+		register
+	);
+router
+	.route('/login')
+	.post(
+		asyncHandler(
+			validate([
+				body('mobile').isMobilePhone().optional(),
+				body('username').isAlphanumeric().optional(),
+				body('email').isEmail().optional(),
+				body('password').isLength({ min: 8 }),
+			])
+		),
+		login
+	);
+
+export default router;
---
+++ server.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import colors from 'colors';
+import morgan from 'morgan';
+import { Database, Middleware } from 'qurba-node-common';
+import userRoutes from './routes/userRoutes.js';
+
+dotenv.config();
+
+Database.connect();
+
+const app = express();
+
+if (process.env.NODE_ENV === 'DEVELOPMENT') {
+	app.use(morgan('dev'));
+}
+app.use(express.json());
+
+// To avoid localhost conflicts from chrome in sending http requests
+app.use(function (req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+	res.setHeader('Access-Control-Allow-Headers', '*');
+
+	res.setHeader('Access-Control-Allow-Credentials', true);
+	next();
+});
+
+app.use('/auth', userRoutes);
+
+// For testing purposes only
+app.get('/', (req, res) => {
+	res.send('Auth API online');
+});
+
+app.use(Middleware.PathHandler);
+app.use(Middleware.ErrorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+	console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.green.bold)
+);
