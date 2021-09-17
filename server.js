6a585255b4fb938d691aa12d0b10e121b8a8c135
Karim-mo <karim.mohamed420@gmail.com>
2021-09-17 02:32:02 +0200

    changed app.listen console log

--- server.js
+++ server.js
@@ -39,5 +39,5 @@ app.use(Middleware.ErrorHandler);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () =>
-	console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.green.bold)
+	console.log(`Auth server running in ${process.env.NODE_ENV} mode on port ${PORT}`.green.bold)
 );
